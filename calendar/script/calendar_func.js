d51bebde70e81dc40173204cece76339185b9718
Jinsol Kim <jinsol032360@gmail.com>
2024-06-16 22:12:40 +0900

    fix some codes

--- calendar/script/calendar_func.js
+++ calendar/script/calendar_func.js
@@ -1,28 +1,63 @@
-const wrapper = document.getElementById('calendar_wrapper');
+const container = document.createElement('div');
+container.id = 'calendar_container';
+document.body.append(container);
+
+const header = document.createElement('div');
+header.id = 'calendar_header';
+container.append(header);
+
+const wrapper = document.createElement('div');
+wrapper.id = 'calendar_wrapper';
+container.append(wrapper);
+
+const schedule = document.createElement('div');
+schedule.id = 'calendar_schedule';
+document.body.append(schedule);
+
 const year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 let month = new Date().getMonth();
 let days = year[month];
 
 function updateCalendar() {
+    // 생명주기가 끝나면 스코프가 다르기 때문에 선언되는게 다른 주소를 참조한다.
+    const monthFirstDay = new Date(new Date().getFullYear(), month, 1).getDay();
+    console.log(month);
+    console.log(monthFirstDay);
     wrapper.innerHTML = '';
 
-    // Month
-    const thisMonth = document.createElement('span');
-    thisMonth.innerHTML = month + 1;
-    wrapper.append(thisMonth);
+    // 월 표시
+    const monthDisplay = document.createElement('span');
+    monthDisplay.id = 'month_display';
+    monthDisplay.innerHTML = month + 1;
+    header.append(monthDisplay);
 
-    // Days
+    // 날짜 생성
+    for (let i = 0; i < monthFirstDay; i++) {
+        const dayDiv = document.createElement('div');
+        dayDiv.innerHTML = '';
+        wrapper.append(dayDiv);
+    }
+
     for (let i = 1; i <= days; i++) {
         const dayDiv = document.createElement('div');
         dayDiv.innerHTML = i;
         wrapper.append(dayDiv);
+        dayDiv.addEventListener('click', () => {
+            // dayDIv 클릭하면 스케쥴 내용을 다큐먼트 바디에 추가
+            // schedule.innerHTML = '로딩중'; // 비동기 실행 전 로딩이나 스켈레톤으로 바로 실행 로직
+            setTimeout(() => {
+                schedule.style.display = 'block';
+                schedule.innerHTML = `${month + 1}월 ${i}일`;
+            }, 1000);
+        });
     }
     appendButtons();
 }
 
 function appendButtons() {
-    // 버튼 : 이전 달
+    header.innerHTML = ''; // 버튼 중복 추가 방지
+
     const pre = document.createElement('button');
     pre.innerHTML = '<';
     pre.addEventListener('click', () => {
@@ -32,9 +67,8 @@ function appendButtons() {
             updateCalendar();
         }
     });
-    wrapper.parentNode.append(pre);
+    header.append(pre);
 
-    // 버튼 : 다음 달
     const next = document.createElement('button');
     next.innerHTML = '>';
     next.addEventListener('click', () => {
@@ -44,7 +78,26 @@ function appendButtons() {
             updateCalendar();
         }
     });
-    wrapper.parentNode.append(next);
+    header.append(next);
+
+    // 월 표시 다시 추가
+    const monthDisplay = document.createElement('span');
+    monthDisplay.id = 'month_display';
+    monthDisplay.innerHTML = month + 1;
+    header.append(monthDisplay);
 }
 
 updateCalendar();
+
+setInterval(() => {
+    if (month < 11) {
+        month++;
+        days = year[month];
+        updateCalendar();
+    } else if ( month >= 11) {
+        month = 0;
+        days = year[month];
+        console.log(month, days);
+        updateCalendar();
+    }
+}, 1000);
--- stopwatch/script/stopwatch.js
+++ stopwatch/script/stopwatch.js
@@ -1,9 +1,13 @@
 const appendTens = document.getElementById('tens');
 const appendSeconds = document.getElementById('seconds');
+const appendMinutes = document.getElementById('minutes');
+const appendHours = document.getElementById('hours');
 const buttonStart = document.getElementById('button-start');
 const buttonStop = document.getElementById('button-stop');
 const buttonReset = document.getElementById('button-reset');
 
+let hours = 0;
+let minutes = 0;
 let seconds = 0;
 let tens = 0;
 let interval;
@@ -13,13 +17,17 @@ buttonStart.onclick = () => {
 };
 
 buttonStop.onclick = () => {
-    clearInterval(interval);
+    clearInterval(interval);  // clearInterval 하지 않으면 스택에 계속 쌓임
 };
 
 buttonReset.onclick = () => {
     clearInterval(interval);
+    hours = 0;
+    minutes = 0;
     seconds = 0;
     tens = 0;
+    appendHours.innerText = 0;
+    appendMinutes.innerText = 0;
     appendSeconds.innerText = 0;
     appendTens.innerText = 0;
 };
